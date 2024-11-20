0df5865bf581129ff20b7a8bc3d70e6fb884aa49
Steffe <156106640+Jason-Goon@users.noreply.github.com>
2024-11-20 03:24:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# Disk preparation
-cfdisk /dev/sda
-mkdir /mnt/gentoo
-mkfs.ext4 /dev/sda3
-mount /dev/sda3 /mnt/gentoo
-mkdir -p /mnt/gentoo/boot/efi
-mkfs.fat -F 32 /dev/sda1
-mount /dev/sda1 /mnt/gentoo/boot/efi
-mkswap /dev/sda2
-swapon /dev/sda2
+source gentoo-config.conf
 
-# Stage tarball and basic compile options ALWAYS EDIT FITH CURRENT
+cfdisk $DISK
+mkdir /mnt/gentoo
+mkfs.ext4 $ROOT_PART
+mount $ROOT_PART /mnt/gentoo
+mkdir -p /mnt/gentoo/boot/efi
+mkfs.fat -F 32 $EFI_PART
+mount $EFI_PART /mnt/gentoo/boot/efi
+mkswap $SWAP_PART
+swapon $SWAP_PART
+
 cd /mnt/gentoo
-wget https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20230705T183202Z/stage3-amd64-openrc-20230705T183202Z.tar.xz
+wget $STAGE3_URL
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 nano -w /mnt/gentoo/etc/portage/make.conf
 
-# Chroot preparation
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
@@ -27,71 +26,54 @@ mount --make-rslave /mnt/gentoo/dev
 mount --bind /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run
 
-
-# Chroot into the new environment
-chroot /mnt/gentoo /bin/bash
+chroot /mnt/gentoo /bin/bash 
 source /etc/profile
-export PS1="(chroot) ${PS1}"
+export PS1="(chroot) \$PS1"
 
-# Configure Portage and system profile
 emerge-webrsync
 emerge --sync
 eselect profile list
-eselect profile set 1
+eselect profile set $PROFILE
 emerge -avuDN @world
 nano -w /etc/portage/make.conf
 echo "USE=\"*\"" >> /etc/portage/make.conf
 echo "ACCEPT_LICENSE=\"*\"" >> /etc/portage/make.conf
-echo "Europe/Helsinki" > /etc/timezone
+echo "$TIMEZONE" > /etc/timezone
 emerge --config sys-libs/timezone-data
-nano -W /etc/locale.gen
+echo "$LOCALE" >> /etc/locale.gen
 locale-gen
 eselect locale list
-eselect profile set 4
-env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
+eselect locale set 1
+env-update && source /etc/profile && export PS1="(chroot) \$PS1"
 emerge --ask sys-kernel/linux-firmware
 emerge sys-kernel/installkernel-gentoo
 emerge sys-kernel/gentoo-kernel-bin
 
-# Generate fstab using genfstab script
-sudo su
-cd
 chmod +x genfstab
 ./genfstab -U /mnt/gentoo >> /mnt/gentoo/etc/fstab
 
-#!/bin/bash
-
-# Set host and system information
-echo "gentoobox" > /etc/conf.d/hostname
-cat > /etc/hosts << EOF
+echo "$HOSTNAME" > /etc/conf.d/hostname
+cat > /etc/hosts << EOL
+127.0.0.1 $HOSTNAME
 127.0.0.1 localhost
-127.0.1.1 gentoobox.localdomain gentoobox
 ::1       localhost ip6-localhost ip6-loopback
 ff02::1   ip6-allnodes
 ff02::2   ip6-allrouters
-EOF
+EOL
 
-# Set the root password
-echo "root:password" | chpasswd
+echo "root:$PASSWORD" | chpasswd
 
-# Set keymap preferences
-echo "keymap=\"us colemak\"" > /etc/conf.d/keymaps
-
+echo "keymap=\"$KEYMAP\"" > /etc/conf.d/keymaps
 emerge dhcpcd sudo neofetch grub efibootmgr 
-
-grub-install /dev/sda
+grub-install $DISK
 grub-mkconfig -o /boot/grub/grub.cfg
-
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers.d/wheel
 chmod 0440 /etc/sudoers.d/wheel
 sed -i '/^%wheel ALL=(ALL) ALL/d' /etc/sudoers
+useradd -m -G wheel,users,audio,video,usb -s /bin/bash $USERNAME
+echo "$USERNAME:$PASSWORD" | chpasswd
 
-
-useradd -m -G wheel,users,audio,video,usb -s /bin/bash vandros
-passwd vandros
-
-# Exit chroot and cleanup
 exit
-exit
-sudo umount -a
+umount -a
+echo "Gentoo installation is complete!"
 
